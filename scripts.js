5f04c14f31989a623f5dcb6a32d073bd57233741
abdurasul-9774 <92166541+abdurasul-9774@users.noreply.github.com>
2021-10-08 18:59:12 +0500

    yukladim

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function parolnikorsat1() {
+    var parol1 = document.getElementById("p1")
+    if (parol1.type == "password"){parol1.type = "text";}
+    else {parol1.type = "password";}
+   
+}
+function parolnikorsat2() {
+    var parol2 = document.getElementById("p2")
+    if (parol2.type == "password"){parol2.type = "text";}
+    else {parol2.type = "password";}
+}
